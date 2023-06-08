0df4d6c6728a9b90a3c5950e839b22ecfe0aa6cc
songzhenliang <1353488639@qq.com>
2023-06-08 17:04:38 +0800

    rocketmq练习

--- mq-produce-server/src/main/java/com/mandala/mq/produce/listener/TransactionListener.java
+++ mq-produce-server/src/main/java/com/mandala/mq/produce/listener/TransactionListener.java
@@ -5,8 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.spring.annotation.RocketMQTransactionListener;
 import org.apache.rocketmq.spring.core.RocketMQLocalTransactionListener;
 import org.apache.rocketmq.spring.core.RocketMQLocalTransactionState;
+import org.apache.rocketmq.spring.support.RocketMQHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Author songzhenliang
@@ -17,6 +22,7 @@ import org.springframework.messaging.Message;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class TransactionListener implements RocketMQLocalTransactionListener {
 
+    private static final Map<String, RocketMQLocalTransactionState> TRANSACTION_STATE_MAP = new HashMap<>();
     /**
      * 处理本地事务
      * @param message
@@ -25,7 +31,23 @@ public class TransactionListener implements RocketMQLocalTransactionListener {
      */
     @Override
     public RocketMQLocalTransactionState executeLocalTransaction(Message message, Object o) {
-        return null;
+        log.info("执行本地事物");
+        MessageHeaders headers = message.getHeaders();
+        String transactionId = (String) headers.get(RocketMQHeaders.TRANSACTION_ID);
+        TRANSACTION_STATE_MAP.put(transactionId, RocketMQLocalTransactionState.UNKNOWN);
+        log.info("transaction id : {}", transactionId);
+        try {
+            Thread.sleep(10 * 1000l);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        RocketMQLocalTransactionState state = RocketMQLocalTransactionState.ROLLBACK;
+        if(Integer.parseInt(transactionId) % 2 == 0){
+            state = RocketMQLocalTransactionState.COMMIT;
+        }
+        log.info("transactionId is {}, state {}",transactionId, state.toString());
+        TRANSACTION_STATE_MAP.remove(transactionId);
+        return state;
     }
 
     /**
@@ -35,6 +57,14 @@ public class TransactionListener implements RocketMQLocalTransactionListener {
      */
     @Override
     public RocketMQLocalTransactionState checkLocalTransaction(Message message) {
-        return null;
+        MessageHeaders headers = message.getHeaders();
+        //获取事务ID
+        String transactionId = (String) headers.get(RocketMQHeaders.TRANSACTION_ID);
+        log.info("检查本地事务,事务ID:{}",transactionId);
+        RocketMQLocalTransactionState state = TRANSACTION_STATE_MAP.get(transactionId);
+        if(null != state){
+            return state;
+        }
+        return RocketMQLocalTransactionState.ROLLBACK;
     }
 }
